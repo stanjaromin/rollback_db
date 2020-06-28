e3d05f5707d95404ff497eca365641edd120eb54
Stanislaw Jaromin <jaromin@onet.pl>
2020-06-28 22:38:39 +0200

    Initial commit

---
+++ jira_test.py
@@ -0,0 +1,9 @@
+from jira import JIRA
+
+jira = JIRA('https://tiswebmaintenance.atlassian.net/')
+auth_jira = JIRA(basic_auth=('maintenance_dmm@globallogic.com', 'qwer-1234'))
+
+issue = jira.issue('TED-85')
+print(issue.fields.project.key)           # 'JRA'
+print(issue.fields.issuetype.name)          # 'New Feature'
+print(issue.fields.reporter.displayName)    # 'Mike Cannon-Brookes [Atlassian]'
