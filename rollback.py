c3dfbd68e869c01c81fac67ebdd344155d4439ca
Stanislaw Jaromin <jaromin@onet.pl>
2020-07-01 22:37:53 +0200

    1st commit

--- rollback.py
+++ rollback.py
@@ -2,10 +2,13 @@ import sqlite3
 import datetime
 import pytz
 
-db = sqlite3.connect("accounts.sqlite")
+db = sqlite3.connect("accounts.sqlite", detect_types=sqlite3.PARSE_DECLTYPES)
 db.execute("CREATE TABLE IF NOT EXISTS accounts (name TEXT PRIMARY KEY NOT NULL, balance INTEGER NOT NULL)")
 db.execute("CREATE TABLE IF NOT EXISTS history (time TIMESTAMP NOT NULL,"
            " account TEXT NOT NULL, amount INTEGER NOT NULL, PRIMARY KEY (time, account))")
+db.execute("CREATE VIEW IF NOT EXISTS localhistory AS"
+           " SELECT strftime('%Y-%m-%d %H:%M:%f', history.time, 'localtime') AS localtime,"
+           " history.account, history.amount FROM history ORDER BY history.time")
 
 
 class Account(object):
@@ -13,6 +16,7 @@ class Account(object):
     @staticmethod
     def _current_time():
         return pytz.utc.localize(datetime.datetime.utcnow())
+        # return 1
 
     def __init__(self, name: str, opening_balance: int = 0):
         cursor = db.execute("SELECT name, balance FROM accounts WHERE (name = ?)", (name,))
@@ -32,10 +36,15 @@ class Account(object):
     def _save_update(self, amount):
         new_balance = self._balance + amount
         deposit_time = Account._current_time()
-        db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
-        db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
-        db.commit()
-        self._balance = new_balance
+
+        try:
+            db.execute("UPDATE accounts SET balance = ? WHERE (name = ?)", (new_balance, self.name))
+            db.execute("INSERT INTO history VALUES(?, ?, ?)", (deposit_time, self.name, amount))
+        except sqlite3.Error:
+            db.rollback()
+        else:
+            db.commit()
+            self._balance = new_balance
 
     def deposit(self, amount: int) -> float:
         if amount > 0.0:
